34bd80a98ef7cd7eb8a808b1fd159005cc95d534
pridi0t <yoeun18@gmail.com>
2023-09-22 12:13:05 +0900

    프로젝트 생성 완료

---
+++ src/main/java/com/popeuling/PopeulingApplication.java
@@ -0,0 +1,13 @@
+package com.popeuling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopeulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopeulingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/popeuling/PopeulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popeuling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopeulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
