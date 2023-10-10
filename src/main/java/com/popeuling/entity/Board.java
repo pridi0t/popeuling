6e07d6ac037b04cd09771d17104c440f94ea8d3e
pridi0t <yoeun18@gmail.com>
2023-10-10 16:51:07 +0900

    Feature: Board member의 JoinColumn nullable 추가

--- src/main/java/com/popeuling/entity/Board.java
+++ src/main/java/com/popeuling/entity/Board.java
@@ -39,6 +39,6 @@ public class Board {
     private String Content;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 }
