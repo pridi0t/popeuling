be564b6c4d862beb84d904ec9a6e81c189fd6381
ljt0815 <ljt0815@naver.com>
2023-10-10 13:39:47 +0900

    Feature: Member, Reply 엔티티 수정 및 MemberRepository, ReplyRepository 추가

--- src/main/java/com/popeuling/entity/Member.java
+++ src/main/java/com/popeuling/entity/Member.java
@@ -1,25 +1,26 @@
 package com.popeuling.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
-@Getter @Setter
+@AllArgsConstructor
+@Getter
+@Setter
 public class Member {
 
-    @Id @GeneratedValue
-    Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
 
     @Column(nullable = false, length = 30, unique = true)
     private String username;
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String nickname;
 
     @Column(nullable = false, length = 60)
     private String password;
--- src/main/java/com/popeuling/entity/Reply.java
+++ src/main/java/com/popeuling/entity/Reply.java
@@ -1,24 +1,31 @@
 package com.popeuling.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Reply {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reply_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
     private Member member;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name="bid")
-//    private Board board;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Board board;
 
     @Column(nullable = false)
     private String content;
